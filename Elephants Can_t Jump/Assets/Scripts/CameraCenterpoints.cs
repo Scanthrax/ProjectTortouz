922a9a0ea8a36a04b3628a27ca20ebf27862e195
Scanthrax <35387121+Scanthrax@users.noreply.github.com>
2018-06-13 16:18:56 -0700

    6/13 update #2

--- Elephants Can_t Jump/Assets/Scripts/CameraCenterpoints.cs
+++ Elephants Can_t Jump/Assets/Scripts/CameraCenterpoints.cs
@@ -26,19 +26,24 @@ public class CameraCenterpoints : MonoBehaviour {
     void ChangeRoom(Transform from, Transform to)
     {
         print("should be changing room");
-        Time.timeScale = 0f;
+
+        // pause the game while room is changing
+        if(Time.timeScale != 0f) Time.timeScale = 0f;
+        // increment lerp
+        lerp += 0.045f;
+        // update camera position
         cam.transform.position = Vector3.Lerp(from.position, to.position, lerp);
-        lerp += 0.08f;
+        // if camera destination reached...
         if (lerp > 1f)
         {
+            // reset lerp for next change
             lerp = 0f;
+            // update camera position
             cam.transform.position = to.position;
+            // set static variable to destination room
             Variables.room = to;
+            // unpause the game
             Time.timeScale = 1f;
         }
-
     }
-
-
-
 }
--- Elephants Can_t Jump/Assets/Scripts/Room.cs
+++ Elephants Can_t Jump/Assets/Scripts/Room.cs
@@ -7,7 +7,6 @@ public class Room : MonoBehaviour {
 
 
     public Transform player;
-    public Rooms thisRoom;
 
     public delegate void RoomChange(Transform fromRoom, Transform toRoom);
     public static event RoomChange roomChange;
@@ -15,17 +14,13 @@ public class Room : MonoBehaviour {
 
     void Update()
     {
-        // outside of room
-        if (player.position.x < transform.position.x - Variables.horzExtent ||
+        // if the player is outside the bounds of the room
+        if (!(player.position.x < transform.position.x - Variables.horzExtent ||
             player.position.x > transform.position.x + Variables.horzExtent ||
             player.position.y < transform.position.y - Variables.vertExtent ||
-            player.position.y > transform.position.y + Variables.vertExtent )
+            player.position.y > transform.position.y + Variables.vertExtent ))
         {
-            print("Outside of " + thisRoom);
-        }
-        // inside of room
-        else
-        {
+            // if new room is entered, change rooms
             if(Variables.room != transform)
             {
                 roomChange(Variables.room, transform);
