869095c6c54eed7624cb7f44f2d28da9607188da
Felipe <Cfherrer@asu.edu>
2018-06-27 12:02:58 -0700

    Platform Code Progress
    About half way done with moving platforms

---
+++ Elephants Can_t Jump/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public GameObject platform;
+
+    public float speed;
+
+    public Transform currentP;
+
+    public Transform[] pointArr;
+
+    public int pointSelection;
+    // Use this for initialization
+    void Start()
+    {
+        currentP = pointArr[pointSelection];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        platform.transform.position = Vector3.MoveTowards(platform.transform.position, currentP.position, Time.deltaTime * speed);
+        if(platform.transform.position == currentP.position)
+        {
+            pointSelection++;
+            if (pointSelection == pointArr.Length)
+            {
+                pointSelection = 0;
+            }
+
+            currentP = pointArr[pointSelection];
+        }
+
+    }
+}
