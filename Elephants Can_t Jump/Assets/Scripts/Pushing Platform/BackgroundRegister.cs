a56af9037e587e03cfc845aa5f20ade8b175304d
Felipe <cfherrer@asu.edu>
2018-07-13 23:38:24 -0700

    Progess on the launching pad
    Commiting so I can pull newer code

---
+++ Elephants Can_t Jump/Assets/Scripts/Pushing Platform/BackgroundRegister.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRegister : MonoBehaviour
+{
+    private GameObject Player;
+    public GameObject Light;
+    private bool start;
+    public float interval = 1;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        start = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            StartCoroutine("cntdwn");
+        }
+    }
+
+    IEnumerator cntdwn()
+    {
+        Debug.Log("Red");
+        Light.GetComponent<lightControl>().TurnRed();
+        yield return new WaitForSeconds(1.5f);
+        Debug.Log("Yellow");
+        Light.GetComponent<lightControl>().TurnYellow();
+        yield return new WaitForSeconds(.1f);
+        Debug.Log("Green");
+        Light.GetComponent<lightControl>().TurnGreen();
+        yield return new WaitForSeconds(.1f);
+    }
+}
