7d50410294af90350069a43fa141d0a5afa03147
Felipe <cfherrer@asu.edu>
2018-07-26 17:19:08 -0700

    Door Prefab added
    Added the prefab for a door. Opens with a button and cannot be closed after that.. Some changes were made to the button script.

--- Elephants Can_t Jump/Assets/Scripts/Button.cs
+++ Elephants Can_t Jump/Assets/Scripts/Button.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Button : MonoBehaviour, IBreakable
 {
+    public bool active; //used to deactivate button for objects that only have 1 function such as doors that only open.
     public bool isPressed; //boolean used by other objects to see if button is pressed or not. USE THIS BOOL TO CHECK STATE OF BUTTON
 
     public bool press; // press/release delay system
@@ -21,6 +22,7 @@ public class Button : MonoBehaviour, IBreakable
     SpriteRenderer spriteRend;
     // Use this for initialization
     void Start () {
+        active = true;
         isPressed = false;
         spriteRend = GetComponent<SpriteRenderer>();
     }
@@ -47,52 +49,54 @@ public class Button : MonoBehaviour, IBreakable
 	// Update is called once per frame
 	void Update ()
     {
-        //if button is pressed
-        if(press == true)
+        if (active)
         {
-            //lerping of button
-            time1 += speed1 * Time.deltaTime;
-            this.transform.position = Vector3.Lerp(this.transform.position, EndP.position, time1);
-            moving = true; //disable pressing
-            isPressed = true;
-            if (this.transform.position == EndP.position) //if button is done moving
+            //if button is pressed
+            if (press == true)
             {
-                moving = false;
-                time1 = 0f;
-                press = false;
+                //lerping of button
+                time1 += speed1 * Time.deltaTime;
+                this.transform.position = Vector3.Lerp(this.transform.position, EndP.position, time1);
+                moving = true; //disable pressing
+                isPressed = true;
+                if (this.transform.position == EndP.position) //if button is done moving
+                {
+                    moving = false;
+                    time1 = 0f;
+                    press = false;
+                }
+
             }
 
-        }
-
-        //if button is pressed a second time
-        if (release == true)
-        {
-            //lerping
-            time2 += speed2 * Time.deltaTime;
-            this.transform.position = Vector3.Lerp(this.transform.position, StartP.position, time2);
-            moving = true; //disable pressing
-            isPressed = false;
-            if (this.transform.position == StartP.position) //if button is done moving
+            //if button is pressed a second time
+            if (release == true)
             {
-                moving = false;
-                time2 = 0f;
-                release = false;
+                //lerping
+                time2 += speed2 * Time.deltaTime;
+                this.transform.position = Vector3.Lerp(this.transform.position, StartP.position, time2);
+                moving = true; //disable pressing
+                isPressed = false;
+                if (this.transform.position == StartP.position) //if button is done moving
+                {
+                    moving = false;
+                    time2 = 0f;
+                    release = false;
+                }
+
             }
 
-        }
-
-        if(isPressed)
-        {
-            //spriteRend.color = Color.gray;
-        }
-        else
-        {
-            //spriteRend.color = Color.red;
+            if (isPressed)
+            {
+                //spriteRend.color = Color.gray;
+            }
+            else
+            {
+                //spriteRend.color = Color.red;
+            }
         }
 
     }
-
-    /*
+    
     private void OnTriggerStay2D(Collider2D other)
     {
        if(other.CompareTag("Player")) //check if the player is in the trigger
@@ -113,5 +117,4 @@ public class Button : MonoBehaviour, IBreakable
         
        }
     }
-    */
 }
---
+++ Elephants Can_t Jump/Assets/Scripts/Pushing Platform/Platform.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    public GameObject Background;
+    private bool shoot;
+    private Rigidbody2D RB;
+    Vector3 direction;
+    public Transform StartP;
+    public Transform EndP;
+
+    //Lerp
+    public float ForwardSpeed;
+    public float backwardSpeed;
+    private float time1;
+    private float time2;
+
+    private bool isAtStart;
+    private bool isAtEnd;
+
+	// Use this for initialization
+	void Start ()
+    {
+        RB = this.GetComponent<Rigidbody2D>();
+        shoot = false;
+        direction = -(this.transform.right);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        shoot = Background.GetComponent<BackgroundRegister>().shoot;
+
+        if(shoot)
+        {
+            
+
+
+
+
+
+        }
+
+        if(this.transform.position == EndP.position)
+        {
+            Debug.Log("here");
+            this.transform.position = EndP.position;
+        }
+    }
+
+    void addForce()
+    {
+        //RB.AddForce(direction * speed);
+    }
+
+}
