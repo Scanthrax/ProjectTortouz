16d101f9253c874e1103c8fd12366120c3e54b8b
Scanthrax <35387121+Scanthrax@users.noreply.github.com>
2018-07-30 11:13:08 -0700

    push 7/30

---
+++ Elephants Can_t Jump/Assets/AlienCollectable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienCollectable : MonoBehaviour {
+
+    public AlienObjects alien;
+    public SpriteRenderer rend;
+
+    private void OnValidate()
+    {
+        if (alien != null)
+        {
+            rend.sprite = alien.sprite;
+        }
+    }
+}
---
+++ Elephants Can_t Jump/Assets/Scripts/AlienObjects.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Alien", menuName = "Alien")]
+public class AlienObjects : ScriptableObject
+{
+    public Sprite sprite;
+    public string name;
+    public bool isCollected;
+}
