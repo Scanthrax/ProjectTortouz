e1ce671c28af83282354907397f34b4943daa8c4
Felipe <Cfherrer@asu.edu>
2018-12-13 15:18:31 -0500

    Added test credit scene. Deleted Button Test scene.
    Added test scrolling credits scene.
    
    deleted button test scene since it is not needed any longer.

---
+++ Elephants Can_t Jump/Assets/Scripts/Credits/Scroll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour {
+
+    public GameObject Credits;
+    public Transform endPoint;
+
+    public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Move credits towards end point
+        Credits.transform.position = Vector3.MoveTowards(Credits.transform.position, endPoint.position, Time.deltaTime*speed);
+	}
+}
